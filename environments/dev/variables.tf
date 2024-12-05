a723c6a9dddd5dd3480e7bc0e5e8b1f7f6bb053c
guilhermeguirro <guiguirro@pm.me>
2024-12-05 18:58:45 -0300

    Update variables.tf

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -1,29 +1,24 @@
 variable "environment" {
   description = "Environment name"
   type        = string
-  default     = "dev"
 }
 
-variable "vpc_cidr" {
-  description = "VPC CIDR block"
+variable "vpc_id" {
+  description = "VPC ID"
   type        = string
-  default     = "10.0.0.0/16"
 }
 
-variable "private_subnets" {
-  description = "Private subnet CIDR blocks"
-  type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24", "10.0.4.0/24"]
-}
-
-variable "public_subnets" {
-  description = "Public subnet CIDR blocks"
-  type        = list(string)
-  default     = ["10.0.101.0/24"]
-}
-
-variable "database_name" {
-  description = "Name of the database to create"
+variable "vpn_cidr" {
+  description = "VPN CIDR block"
   type        = string
-  default     = "appdb"
+}
+
+variable "vpc_cidrs" {
+  description = "List of VPC CIDR blocks"
+  type        = list(string)
+}
+
+variable "stacksync_ips" {
+  description = "List of Stacksync IP addresses"
+  type        = list(string)
 }
